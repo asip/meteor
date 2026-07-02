d1abf059529ba71f258f7bc4ada823a927480050
Yasumasa Ashida <ys.ashida@gmail.com>
2026-07-02 20:40:09 +0900

    chore: release v0.9.30

--- lib/meteor.rb
+++ lib/meteor.rb
@@ -36,11 +36,11 @@ require "meteor/ml/xml/parser_impl"
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # @author Yasumasa Ashida
-# @version 0.9.29
+# @version 0.9.30
 #
 
 module Meteor
-  VERSION = "0.9.29"
+  VERSION = "0.9.30"
 
   # require 'fileutils'
 
